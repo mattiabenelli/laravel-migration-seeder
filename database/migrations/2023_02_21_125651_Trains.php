c2f28d73548256946d8b5a790b85bf7ef28c0501
mattiabenelli <benellimattia0@gmail.com>
2023-02-21 15:41:15 +0100

    tabella generata

--- database/migrations/2023_02_21_125651_Trains.php
+++ database/migrations/2023_02_21_125651_Trains.php
@@ -15,7 +15,7 @@ return new class extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
-            // $table->timestamps();
+            $table->timestamps();
             $table->string('azienda', 60);
             $table->string('stazione_di_partenza', 60);
             $table->string('stazione_di_arrivo', 60);
